93f4e49e30589df9e50d1f9dddf6fed3191c2b7f
IsaiSG1 <isaisilva11@gmail.com>
2024-11-03 22:00:55 -0800

    Completed JSE variables lab

---
+++ index.js
@@ -0,0 +1,17 @@
+let age = 25;
+age = 27;
+console.log("My age is: ", age);
+
+const birthYear = 1996;
+console.log("I was born in: ", birthYear);
+
+//applying variables in expressions
+let currentYear = 2024;
+let yearsSinceBirth = currentYear -birthYear;
+console.log("years since birth:", yearsSinceBirth);
+
+const greeting ="hello";
+let userName = "John";
+userName ="Bob";
+let personalizedGreeting = greeting + "," + userName + "!";
+console.log(personalizedGreeting);
